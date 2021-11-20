e3c3f0c2d91edde2855df3f6b634f638f4995229
nime0110 <77383149+nime0110@users.noreply.github.com>
2021-11-20 17:31:30 +0900

    구구단 4단 구현

--- src/S_gugudan_4.java
+++ src/S_gugudan_4.java
@@ -1,4 +1,39 @@
+import java.util.Scanner;
 
 public class S_gugudan_4 {
-
+	public static void main(String[] orgs) {
+	//4단
+	System.out.println("4단");
+	int result = 4 * 1;
+	System.out.println(result);
+	result = 4 *2;
+	System.out.println(result);
+	result = 4 *3;
+	System.out.println(result);
+	result = 4 *4;
+	System.out.println(result);
+	result = 4 *5;
+	System.out.println(result);
+	result = 4 *6;
+	System.out.println(result);
+	result = 4 *7;
+	System.out.println(result);
+	result = 4 *8;
+	System.out.println(result);
+	result = 4 *9;
+	System.out.println(result);
+	
+	System.out.println("구구단 중 출력할 단은?");
+	Scanner scanner = new Scanner(System.in);
+	int number = scanner.nextInt();
+	System.out.println(number * 1);
+	System.out.println(number * 2);
+	System.out.println(number * 3);
+	System.out.println(number * 4);
+	System.out.println(number * 5);
+	System.out.println(number * 6);
+	System.out.println(number * 7);
+	System.out.println(number * 8);
+	System.out.println(number * 9);	
+	}
 }
