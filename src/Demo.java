faa026c5c6c950db0c6216f7d61abed12dd1134e
nime0110 <77383149+nime0110@users.noreply.github.com>
2021-11-20 13:37:35 +0900

    구구단 2,3단 출력

--- src/Demo.java
+++ src/Demo.java
@@ -1,7 +1,16 @@
-
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("hello java");
-		System.out.println("hi java");
+		int number1 = 2;
+		int number2 = 3;
+		System.out.println("<구구단 2단 출력>");
+		for(int i=1; i<10; i++) {
+			System.out.println("2x" + i + "=" + number1 * i);
+			if(i == 9) {
+				System.out.println("<구구단 3단 출력>");
+				for(int j=1; j<10; j++) {
+				System.out.println("3x" + j + "=" + number2 * j);
+			}
+		}
+	}
 	}
 }
