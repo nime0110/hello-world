5c13c94409e9ebc3c949f03cafb4d4978547e9c7
nime0110 <77383149+nime0110@users.noreply.github.com>
2021-11-20 21:26:08 +0900

    구구단 9단 else if, else를 이용하여 구현

--- src/Gugudan_9.java
+++ src/Gugudan_9.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 //Scanner 호출
 public class Gugudan_9 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		System.out.println("구구단 중 출력할 단은?(숫자만 입력하시오.) : ");
+		Scanner sc = new Scanner(System.in);
 		int userNumber = sc.nextInt();
-		for(int i=1; i<10; i++) {
-			if(userNumber == 1) {				
-				System.out.println("2이상, 9이하의 값만 입력할 수 있습니다. ");
-				break;
+		System.out.println("사용자가 입력한 값 : " + userNumber);
+		
+		if(userNumber < 2) {				
+			System.out.println("2이상, 9이하의 값만 입력할 수 있습니다. ");
+		} else if (userNumber > 9) {
+			System.out.println("2이상, 9이하의 값만 입력할 수 있습니다. ");				
+		} else {			
+			for(int i=1; i<10; i++) {
+				System.out.println(userNumber * i);
 			}
-			if(userNumber > 9 ) {				
-				System.out.println("2이상, 9이하의 값만 입력할 수 있습니다. ");
-				break;
-			}
-			System.out.println(userNumber * i);
 		}
 	}
 }
